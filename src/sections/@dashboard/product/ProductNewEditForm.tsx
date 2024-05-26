711d81d7dd04747d688e826364c40b345618039e
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2024-05-27 00:12:28 +0330

    feat: product from add feature

--- src/sections/@dashboard/product/ProductNewEditForm.tsx
+++ src/sections/@dashboard/product/ProductNewEditForm.tsx
@@ -7,7 +7,7 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 // @mui
 import { LoadingButton } from '@mui/lab';
-import { Box, Card, Grid, Stack, Switch, Typography, FormControlLabel } from '@mui/material';
+import { Box, Card, Grid, Stack, Switch, Typography, FormControlLabel, MenuItem, FormLabel, IconButton, Button } from '@mui/material';
 // utils
 import { fData } from '../../../utils/formatNumber';
 // routes
@@ -30,6 +30,9 @@ import {
 } from '../../../components/hook-form';
 import AxiosApi from 'src/utils/axios';
 import { BrandDto, CategoryDto, ProductDto, RoleType } from 'src/@types/models';
+import { DialogStateType } from 'src/components/custom/CustomDialog';
+import { ProductFeatureDialog } from './ProductFeatureDialog';
+import Iconify from 'src/components/Iconify';
 
 // ----------------------------------------------------------------------
 
@@ -83,6 +86,10 @@ export default function ProductNewEditForm({ isEdit, currentProduct }: Props) {
 
   const [brands, setBrands] = useState<BrandDto[]>([]);
   const [categories, setCategories] = useState<CategoryDto[]>([]);
+
+  // ALIREZAAAAAAA 
+  // use ReactQuery for this requests
+  // Like hooks I'm using in Lists
   useEffect(() => {
     AxiosApi.categoryList({})
       .then((res) => {
@@ -92,7 +99,9 @@ export default function ProductNewEditForm({ isEdit, currentProduct }: Props) {
         console.error(err);
       });
   }, []);
-
+  // ALIREZAAAAAAA 
+  // use ReactQuery for this requests
+  // Like hooks I'm using in Lists
   useEffect(() => {
     AxiosApi.brandList()
       .then((res) => {
@@ -102,6 +111,16 @@ export default function ProductNewEditForm({ isEdit, currentProduct }: Props) {
         console.error(err);
       });
   }, []);
+
+  const [features, setFeatures] = useState<string[]>([]);
+
+  const handleAddFeature = (feature: string) => {
+    setFeatures((prev) => ([...prev, feature]))
+  }
+
+  const handleRemoveFeature = (selectedFeature: string) => {
+    setFeatures((prev) => prev.filter((feature) => feature !== selectedFeature))
+  }
 
   const methods = useForm<FormValuesProps>({
     resolver: yupResolver(NewUserSchema),
@@ -141,6 +160,20 @@ export default function ProductNewEditForm({ isEdit, currentProduct }: Props) {
     }
   };
 
+  const [openFormDialog, setOpenFormDialog] = useState<
+    DialogStateType<{}>
+  >({
+    open: false,
+  });
+
+  const handleOpenAddDialog = () => {
+    setOpenFormDialog({ open: true });
+  };
+
+  const handleCloseDialog = () => {
+    setOpenFormDialog({ open: false });
+  };
+
   const handleCreateProduct = async (data: FormValuesProps) => {
     AxiosApi.postProduct({ data });
   };
@@ -172,49 +205,76 @@ export default function ProductNewEditForm({ isEdit, currentProduct }: Props) {
   };
 
   return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Card sx={{ p: 3 }}>
-        <Box
-          sx={{
-            display: 'grid',
-            columnGap: 2,
-            rowGap: 3,
-            gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
-          }}
-        >
-          {/* attributes */}
-          <RHFTextField name="title" label="Title" />
-          <RHFTextField name="summery" label="Summery" />
-          <RHFTextField name="description" label="Description" />
-          <RHFSelect name="categoryId" label="Category">
-            {categories.map((option) => (
-              <option key={option.id} value={option.id}>
-                {option.title}
-              </option>
-            ))}
-          </RHFSelect>
-          <RHFSelect name="brandId" label="Brand">
-            {brands.map((option) => (
-              <option key={option.id} value={option.id}>
-                {option.title}
-              </option>
-            ))}
-          </RHFSelect>
-          <Box />
-          <RHFUploadMultiFile
-            name="files"
-            onDrop={handleDrop}
-            onRemove={handleRemove}
-            onRemoveAll={handleRemoveAll}
-          />
-        </Box>
+    <>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Card sx={{ p: 3 }}>
+          <Box
+            sx={{
+              display: 'grid',
+              columnGap: 2,
+              rowGap: 3,
+              gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
+            }}
+          >
+            {/* attributes */}
+            <RHFTextField name="title" label="Title" />
+            <RHFTextField name="summery" label="Summery" />
+            <RHFTextField name="description" label="Description" />
+            <RHFSelect name="categoryId" label="Category">
+              {categories.map((option) => (
+                <MenuItem key={option.id} value={option.id}>
+                  {option.title}
+                </MenuItem>
+              ))}
+            </RHFSelect>
+            <RHFSelect name="brandId" label="Brand">
+              {brands.map((option) => (
+                <MenuItem key={option.id} value={option.id}>
+                  {option.title}
+                </MenuItem>
+              ))}
+            </RHFSelect>
+            <Box />
+            <RHFUploadMultiFile
+              name="files"
+              onDrop={handleDrop}
+              onRemove={handleRemove}
+              onRemoveAll={handleRemoveAll}
+            />
+            <Box />
+            <Stack spacing={2} mt={2}>
+              <FormLabel>
+                Features
+              </FormLabel>
+              {features.map((feature) => (
+                <Stack key={feature} direction="row" alignItems="center" justifyContent="space-between">
+                  <Typography variant='body1' flexGrow={1} noWrap>
+                    {feature}
+                  </Typography>
+                  <IconButton color="error">
+                    <Iconify icon="eva:trash-2-outline" width={24} height={24} onClick={() => {
+                      handleRemoveFeature(feature);
+                    }} />
+                  </IconButton>
+                </Stack>
+              ))}
+              <Box>
+                <Button variant='outlined' color='primary' startIcon={<Iconify icon="ic:round-add" width={24} height={24} />} onClick={handleOpenAddDialog}>
+                  Add New Feature
+                </Button>
+              </Box>
+            </Stack>
+            <Box />
+          </Box>
 
-        <Stack alignItems="flex-end" sx={{ mt: 3 }}>
-          <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-            {!isEdit ? 'Create User' : 'Save Changes'}
-          </LoadingButton>
-        </Stack>
-      </Card>
-    </FormProvider>
+          <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+            <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+              {!isEdit ? 'Create User' : 'Save Changes'}
+            </LoadingButton>
+          </Stack>
+        </Card>
+      </FormProvider>
+      <ProductFeatureDialog dialogState={openFormDialog} handleCloseDialog={handleCloseDialog} onSelect={handleAddFeature} />
+    </>
   );
 }
